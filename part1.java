2e169f0935cbe749ec15e40482122f88bec14a48
Ryanlys <43183782+Ryanlys@users.noreply.github.com>
2020-11-10 19:49:11 -0800

    Update part1.java

--- part1.java
+++ part1.java
@@ -9,9 +9,9 @@ import java.util.concurrent.TimeUnit;
 
 public class part1 
 {
-	public static void main(String args[])
+	public static void main(String args[]) //main driver function to get user input and create threads
 	{
-		
+		//get input
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the number of slave threads: \n");
 		int size = sc.nextInt();
@@ -19,38 +19,31 @@ public class part1
 		int maxDur = sc.nextInt();
 		System.out.println("Enter the interval of request generation: \n");
 		int interval = sc.nextInt();	
-		Buffer b = new Buffer(maxDur, interval);
+		Buffer b = new Buffer(maxDur, interval); //initialize variables
 		for (int i = 0; i < size; i++)
 		{
 			new Buffer(i);  //create slaves
 		}
-		Buffer bb = new Buffer();
+		b.producer(); //run producer method
 		
 	}
 
 }
 
 
-class Buffer implements Runnable
+class Buffer implements Runnable //main threading class with consumer and producer functions
 {
-	private static Object test;
+	//private variables
 	private String slaveID;
 	private static int[][] buff = new int[10][2];
 	private static int duration, maxDur, interval, used, empty, reqID; 
 	Thread t;
 	//condition variables and lock
-	private final ReentrantLock lock = new ReentrantLock();
-    private final Condition isFull = lock.newCondition(); 
-    private final Condition isEmpty = lock.newCondition();
+	private static final ReentrantLock lock = new ReentrantLock();
+    private static final Condition isFull = lock.newCondition(); 
+    private static final Condition isEmpty = lock.newCondition();
     
-    public Buffer()
-    {
-    	slaveID = "producer";
-    	t = new Thread(this, "producer"); //create slave thread
-    	t.start();
-    	
-    }
-	public Buffer(int dur, int val) //initialize variables
+	public Buffer(int dur, int val) //Producer constructor to initialize variables
 	{
 		reqID = 1;
 		duration = 0;
@@ -64,7 +57,7 @@ class Buffer implements Runnable
 		}
 	}
 	
-	public Buffer(int id)
+	public Buffer(int id) //Consumer constructor to run consumer function
 	{
 		slaveID = Integer.toString(id);
 		t = new Thread(this, slaveID); //create slave thread
@@ -73,23 +66,20 @@ class Buffer implements Runnable
 	}
 
 	@Override
-	public void run() //consumer function
+	public void run() //Consumer function called by thread.start(), accepts request and sleeps
 	{
-		int[] data = new int[2];
-		if (this.slaveID == "producer")
-		{
-			producer();
-		}		
-		
+		int[] data = new int[2];		
 		while(true)
 		{
-			/*lock.lock();
+			//Atomically check queue status
+			lock.lock(); 
 			try
 			{
 				if (empty == 10)
 				{
-					System.out.println("slave waiting");
+					System.out.println("Queue empty consumer waiting");
 					isEmpty.await(); //queue is empty so sleep until producer makes something
+					System.out.println("Consumer " + this.slaveID +  " woken");
 				}
 				
 			} 
@@ -100,54 +90,45 @@ class Buffer implements Runnable
 				lock.unlock();
 			} 
 			
-			*/
-			boolean flag = false;
+			//Access the queue to get request
+			data = accessBuff(0, false);
+			System.out.println("Consumer " + this.slaveID + " accepted Request " + Integer.toString(data[0]) + " of duration " + Integer.toString(data[1]) + " at time " + LocalTime.now());
+			//Process request by sleeping
 			try
 			{
-				TimeUnit.SECONDS.sleep(3);
-				flag = checkBuff();
+				TimeUnit.SECONDS.sleep(data[1]);
+				System.out.println("Consumer " + this.slaveID + " finished Request " + Integer.toString(data[0]) + " of duration " + Integer.toString(data[1]) + " at time " + LocalTime.now());
+
 			}
 			catch (InterruptedException e)
 			{}
-			
-			if (flag)
-			{
-				data = accessBuff(0, false);
-				System.out.println("Slave " + this.slaveID + " accepted Request " + Integer.toString(data[0]) + " of duration " + Integer.toString(data[1]) + " at time " + LocalTime.now());
-			}
-			
-			try
-			{
-				TimeUnit.SECONDS.sleep(data[1]);	
-			}
-			catch (InterruptedException e)
-			{}
-			
+			//Signal to producer that there is free slot in queue
 			lock.lock();
 			try
 			{
-				isFull.signal(); //signal to producer that there is free slot
+				isFull.signal(); 
 			}
 			finally
 			{
 				lock.unlock();
 			}
+				
 		}
-		
-
 	}
 	
-	public void producer() 
+	public void producer() //Producer method to generate requests
 	{
-		int[] test;
 		while(true)
 		{
+			//Check if queue is full, if full then wait for consumer signal
 			lock.lock();
 			try
 			{
 				if (used == 10)
 				{
+					System.out.println("Queue full producer waiting");
 					isFull.await(); //queue is full so sleep until a slave finishes a request
+					System.out.println("Producer woken");
 				}
 			} catch (InterruptedException e) 
 			{}
@@ -155,18 +136,19 @@ class Buffer implements Runnable
 			{
 				lock.unlock();
 			}
-
+			
+			//Generate request
 			int range = maxDur;
 			duration = (int)(Math.random() * range) + 1;
-			test = accessBuff(duration, true);
+			accessBuff(duration, true); //Insert request to queue
 			System.out.println("Created request " + Integer.toString(reqID) + " of duration " + Integer.toString(duration) + " at time " + LocalTime.now());
 			reqID++;
 			
-			
+			//Wake a random consumer to process the request
 			lock.lock();
 			try
 			{
-				isEmpty.signal(); //wake one consumer
+				isEmpty.signal();
 			}
 			catch (Exception e)
 			{}
@@ -175,9 +157,10 @@ class Buffer implements Runnable
 				lock.unlock();
 			}
 			
+			//Sleep for interval seconds before generating another request
 			try
 			{
-				TimeUnit.SECONDS.sleep(interval); //sleep for interval
+				TimeUnit.SECONDS.sleep(interval); 
 			}
 			catch (InterruptedException e)
 			{}
@@ -185,10 +168,11 @@ class Buffer implements Runnable
 		
 	}
 	
-	private synchronized int[] accessBuff(int dur, boolean producer) //method to access the queue
+	private synchronized int[] accessBuff(int dur, boolean producer) //Method to access the queue, insert request or extract request
 	{
+		
 		int[] ret = new int[2];
-		if (producer) // is producer
+		if (producer) //If is producer
 		{
 			for (int i = 0; i < 10; i++)
 			{
@@ -198,14 +182,15 @@ class Buffer implements Runnable
 					++used;
 					buff[i][0] = reqID;
 					buff[i][1] = dur;
+					ret[0] = 1;
 					return ret;
 				}
 				
 			}
 		}
-		else
+		else //If is consumer
 		{
-			for (int i = 0; i < 10; i++)
+			for (int i = 0; i < 10; i++) //Loop through queue
 			{
 				if (buff[i][0] != 0) //if reqID != 0 then there's a request
 				{
@@ -214,30 +199,13 @@ class Buffer implements Runnable
 					ret[0] = buff[i][0];
 					ret[1] = buff[i][1];
 					buff[i][0] = 0; //remove that request from queue
-					return ret;
+					return ret; //return the reqID and duration
 				}
 				
 			}
-		}
-		
+		}	
 		return ret;
 	}
 	
-	private synchronized boolean checkBuff()
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			if (buff[i][0] != 0) //if reqID = 0 then there's nothing in it
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-			
-		}
-		return false;
-	}
 	
 }
